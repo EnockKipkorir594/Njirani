34013cbefaeaddb6ee9d7b9cecf9be802db60077
Enock <kipkorirenock594@gmail.com>
2026-07-29 13:02:54 +0300

    feat: cetralized error handling.Fixes #17.

--- Typescript-fundamentals/expressServer/src/index.ts
+++ Typescript-fundamentals/expressServer/src/index.ts
@@ -6,28 +6,22 @@ const app = express();
 
 const port = 3000;
 
-//routes
+//Global middleware 
+app.use(express.json());
 
+//routes
 app.get('/', (req, res) => {
     res.send('Hello Express');
 });
-
 app.get('/health', (req, res) => {
     res.json({status : 'ok'});
 });
-app.use(express.json());
-app.post('/echo', (req: Request, res: Response) => {
+app.post('/echo', (req, res) => {
     res.json(req.body);
 });
 
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-
-});
-
-//Zod schema 
+//Zod schema instad of using interface 
 const createUserSchema = z.object({
     name : z.string().min(1),
     email : z.string().email(),
@@ -41,22 +35,23 @@ function validateBody(schema : z.ZodSchema){
         const result = schema.safeParse(req.body);
 
         if (!result.success){
-        res.status(400).json({  
-            error : "Validation failed",
-            details  : result.error.issues
-        });
-        return ;
+            const error = new Error('Validation Error');
+            (error as any).status = 400;
+            (error as any).details =  result.error.issues;
+            next(error);
+            return;
 
-    }   
+    };
         req.body = result.data
         next();
+        
     }
 }
 const users = [
     {id: 1, name: "Enock"},
     {id :2, name: "Mariah"}
 ]
-
+//Implementing Zod validation middleware
 app.post('/users',validateBody(createUserSchema), (req, res) => {
     const newUser = {
         id: users.length + 1, 
@@ -68,4 +63,25 @@ app.post('/users',validateBody(createUserSchema), (req, res) => {
     users.push(newUser);
     res.status(201).json(newUser);
 
+});
+
+//404 error handler 
+app.use(( req,res,next) => {
+    const error = new Error(`Route ${req.method} ${req.path} not found`);
+    (error as any ).status = 404;
+    next(error)
+}) 
+
+//Error handling middleware 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status  || 500;
+    const message = err.message || "Internal server error";
+    res.status(status).json({
+        error: message,
+        details: err.details || undefined
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
