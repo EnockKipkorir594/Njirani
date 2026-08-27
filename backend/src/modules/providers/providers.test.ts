5664786efaa1e0ef6f42ce1380cf7a99b1844689
Enock <kipkorirenock594@gmail.com>
2026-08-27 17:22:28 +0300

    bug: fixed lint unused variable.

--- backend/src/modules/providers/providers.test.ts
+++ backend/src/modules/providers/providers.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import { createTestApp } from '../../tests/helpers/app.js'
 import prisma from '../../config/database.js'
 import { signAccessToken } from '../../utils/jwt.js'
-import { cleanDatabase } from '../../tests/helpers/cleanup.js'
+
 
 const app = createTestApp()
 
